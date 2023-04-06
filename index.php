2e911774bc89db92e1682bc46a0fcbbf4578c947
larson cop <97100495@st.deltion.nl>
2023-04-06 14:05:31 +0200

    update

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <script src="app.js" href="utf-8"></script>
+    <meta http-equiv="X-UA-Compatible" content="IE-edge" />
+    <meta name="viewport" content="width=device-width, initial-scale 1.0" />
+    <link rel="stylesheet" href="style.css" />
+
+    <title>GameHub</title>
+  </head>
+<body>
+<header>
+<a href="#" class="logo">GameHub</a>
+<nav class="navbar">
+<ul>
+<li><a href="#" class="games">Games</a></li>
+<li><a href="#">Home</a></li>
+<li><a href="#">Inloggen</a></li>
+<li><a href="#">Registreren</a></li>
+
+
+
+
+</ul>
+</nav>
+</header>
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
