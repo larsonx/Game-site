4716be538dea30b50f9112c56c4560a7ee72ac55
larson cop <97100495@st.deltion.nl>
2023-04-13 13:31:35 +0200

    changes

--- Website/script.js
+++ Website/script.js
@@ -5,14 +5,18 @@ toggleButton.addEventListener("click", () => {
   navbarLinks.classList.toggle("active");
 });
 
-const wrapper = document.querySelector('.wrapper');
-const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.register-link');
+const wrapper = document.querySelector(".wrapper");
+const loginLink = document.querySelector(".login-link");
+const registerLink = document.querySelector(".register-link");
 
+<<<<<<< Updated upstream
 registerLink.addEventListener('click' , () => {
   wrapper.classList.add('active');
+=======
+registerLink.addEventListener("click", () => {
+  wrapper.classList.add("active");
+>>>>>>> Stashed changes
 });
-loginLink.addEventListener('click' , () => {
-  wrapper.classList.remove('active');
+loginLink.addEventListener("click", () => {
+  wrapper.classList.remove("active");
 });
-
