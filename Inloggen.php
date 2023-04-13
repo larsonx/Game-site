afc0f22d1d00e66414eb3fcd99ef90173d480e69
larson cop <97100495@st.deltion.nl>
2023-04-13 14:39:30 +0200

    update

---
+++ Inloggen.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE-edge" />
+  <meta name="viewport" content="width=device-width, initial-scale 1.0" />
+  <link rel="stylesheet" href="style.css" />
+  <script src="script.js" defer></script>
+  <title>GameHub</title>
+</head>
+
+<body>
+  <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+  <script src="script.js" defer></script>
+  <header>
+    <nav class="navbar">
+      <div class="brand-title">GameHub</div>
+      <a href="#" class="toggle-button">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+      </a>
+      <div class="navbar-links">
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="games.php">Games</a></li>
+          <li><a href="inloggen.php">Login</a></li>
+        </ul>
+    </nav>
+    </div>
+  </header>
+  <div class="wrapper">
+    <div class="form-box login">
+      <h2>Login</h2>
+      <form action="#">
+        <div class="input-box">
+          <span class="icon"><ion-icon name="mail-outline"></ion-icon></ion-icon></span>
+          <input class="kleur" type="email" required>
+          <label>Email</label>
+        </div>
+        <div class="input-box">
+          <span class="icon"><ion-icon name="lock-closed-outline"></ion-icon></ion-icon></span>
+          <input class="kleur" type="password" required>
+          <label>Password</label>
+        </div>
+        <div class="remember-forgot">
+          <label><input type="checkbox">
+            Remember me</label>
+          <a href="#">Forgot Password?</a>
+        </div>
+        <button type="submit" class="login-button">Login
+        </button>
+        <div class="login-register">
+          <p>Don't have an account? <a href="#" class="register-link">Register</a></p>
+        </div>
+      </form>
+    </div>
+    <div class="form-box register">
+      <h2>Registration</h2>
+      <form action="#">
+        <div class="input-box">
+          <span class="icon"><ion-icon name="person-outline"></ion-icon></ion-icon></ion-icon></span>
+          <input class="kleur" type="text" required>
+          <label>Username</label>
+        </div>
+        <form action="#">
+          <div class="input-box">
+            <span class="icon"><ion-icon name="mail-outline"></ion-icon></ion-icon></span>
+            <input class="kleur" type="email" required>
+            <label>Email</label>
+          </div>
+          <div class="input-box">
+            <span class="icon"><ion-icon name="lock-closed-outline"></ion-icon></ion-icon></span>
+            <input class="kleur" type="password" required>
+            <label>Password</label>
+          </div>
+          <div class="remember-forgot">
+            <label><input type="checkbox">
+              I agree to the terms & conditions</label>
+
+          </div>
+          <button type="submit" class="login-button">Register
+          </button>
+          <div class="login-register">
+            <p>Already have an account? <a href="#" class="login-link">Login</a></p>
+          </div>
+        </form>
+    </div>
+  </div>
+
+</body>
+
+</html>
---
+++ script.js
@@ -0,0 +1,19 @@
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
+
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+
+
+registerLink.addEventListener('click' , () => {
+  wrapper.classList.add('active');
+});
+loginLink.addEventListener('click' , () => {
+  wrapper.classList.remove('active');
+});
+
