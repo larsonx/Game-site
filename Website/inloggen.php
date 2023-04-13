ba12b7b4e61615cca027cede9117f46b5eb71621
larson cop <97100495@st.deltion.nl>
2023-04-13 13:11:33 +0200

    ;

--- Website/inloggen.php
+++ Website/inloggen.php
@@ -9,7 +9,6 @@
   <script src="script.js" defer></script>
   <title>GameHub</title>
 </head>
-
 <body>
   <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
   <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
