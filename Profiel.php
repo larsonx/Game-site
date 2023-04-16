a803ace4311e7baa63745ab44eaf8190120223d2
larsonx <97100495@st.deltion.nl>
2023-04-16 17:53:17 +0200

    updatetje profiel pagina

---
+++ Profiel.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE-edge" />
+    <meta name="viewport" content="width=device-width, initial-scale 1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <script src="script.js" defer></script>
+    <title>GameHub</title>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar">
+            <div class="brand-title">GameHub</div>
+            <a href="#" class="toggle-button">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </a>
+            <div class="navbar-links">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="games.php">Games</a></li>
+                    <li><a href="inloggen.php">Login</a></li>
+                </ul>
+        </nav>
+        </div>
+    </header>
+        <div class="container">
+            <div class="profile-header">
+                <div class="profile-image">
+                    <img src="https://via.placeholder.com/150" alt="Profile Image">
+                </div>
+                <div class="profile-username">
+                    <h1>Username</h1>
+                </div>
+            </div>
+            <div class="profile-bio">
+                <h2>Biography</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at orci sit amet augue mollis vestibulum ac vitae elit. Nulla auctor aliquet nisi, eu pellentesque neque pharetra in.</p>
+            </div>
+            <div class="profile-achievements">
+                <h2>Achievements</h2>
+                <ul>
+                    <li>Completed 10 online courses</li>
+                    <li>Published a research paper in a scientific journal</li>
+                    <li>Received a scholarship for academic excellence</li>
+                </ul>
+            </div>
+        </div>
+</body>
+</html>
