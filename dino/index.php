2cc5d7069d4dd8076ab5c5a46db60a3f037811b1
Danila_gruznev <302860771@student.rocmondriaan.nl>
2023-04-20 14:02:00 +0200

    dino spel

---
+++ dino/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="/dino/dino.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Rubik+Vinyl&family=Teko:wght@500&family=Ubuntu+Condensed&display=swap"
+      rel="stylesheet"
+    />
+    <title>Document</title>
+  </head>
+
+  <body>
+    <div class="game">
+      <img id="character" src="/dino/img/dino.png" />
+      <img id="block" src="/dino/img/cactus.png" />
+    </div>
+    <p>Score <span id="scoreSpan"></span></p>
+    <script src="/dino/js/main.js"></script>
+  </body>
+</html>
