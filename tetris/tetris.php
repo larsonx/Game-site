b85b3a63d85a64bb3902fe0054b05f3021dced65
larsonx <97100495@st.deltion.nl>
2023-05-09 10:45:55 +0200

    Update tetris.php

--- tetris/tetris.php
+++ tetris/tetris.php
@@ -16,9 +16,24 @@
 </head>
 
 <body>
-<?php
-include '../header.php';
-  ?>
+<header>
+        <nav class="navbar">
+            <div class="brand-title">GameHub</div>
+            <a href="#" class="toggle-button">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </a>
+            <div class="navbar-links">
+                <ul>
+                    <li><a href="../index.php">Home</a></li>
+                    <li><a href="../highscore.php">Hi-Score</a></li>
+                    <li><a href="../games.php">Games</a></li>
+                    <li><a href="../inloggen.php">Login</a></li>
+                </ul>
+        </nav>
+        </div>
+    </header>
   <div class="container-t">
       
       <div class="grid-t">
