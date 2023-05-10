5a16e3ce06e69a7304f6b943521560a196d8f1d7
Danila_gruznev <302860771@student.rocmondriaan.nl>
2023-05-10 13:48:52 +0200

    dino werkt goed

--- dino.php
+++ dino.php
@@ -1,25 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="dino.css" />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Rubik+Vinyl&family=Teko:wght@500&family=Ubuntu+Condensed&display=swap"
-      rel="stylesheet"
-    />
-    <title>Document</title>
-  </head>
 
-  <body>
-    <div class="game">
-      <img id="character" src="../img/dino copy.png" />
-      <img id="block" src="../img/cactus.png" />
-    </div>
-    <p>Score <span id="scoreSpan"></span></p>
-    <script src="/dino/js/main.js"></script>
-  </body>
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="dino.css" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Rubik+Vinyl&family=Teko:wght@500&family=Ubuntu+Condensed&display=swap"
+    rel="stylesheet" />
+  <title>Document</title>
+</head>
+
+<body>
+  <div class="game">
+    <img id="character" src="../N-N-N-N/img/dino copy.png" />
+    <img id="block" src="../N-N-N-N/img/cactus.png" />
+  </div>
+  <p>Score <span id="scoreSpan"></span></p>
+  <script src="../N-N-N-N/dino/js/main.js"></script>
+</body>
+
 </html>
