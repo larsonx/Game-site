6e20fc60ae445dab3cfa51b0c7da6b3caf22548d
larson cop <97100495@st.deltion.nl>
2023-05-10 12:12:59 +0200

    Update games.php

--- games.php
+++ games.php
@@ -73,7 +73,7 @@ include 'header.php';
       <p>
         a word game in which one player has to guess a word that the other player has thought of, by guessing the letters in it.
       </p>
-      <a href="#" class="button">
+      <a href="galgje/index.php" class="button">
         Play now
       </a>
     </div>
