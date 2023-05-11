43b341ac093a9a7a258a560688b50d36daff8f62
larsonx <97100495@st.deltion.nl>
2023-05-11 10:55:16 +0200

    logo gamehub fix

--- header2.php
+++ header2.php
@@ -1,17 +1,18 @@
 <header>
         <nav class="navbar">
-            <div class="brand-title">GameHub</div>
-            <a href="#" class="toggle-button">
-                <span class="bar"></span>
-                <span class="bar"></span>
-                <span class="bar"></span>
-            </a>
+        <div class="brand-title">
+        <span class="span1">Game</span>
+        <span class="span2">Hub</span>
+      </div>
+      <a href="#" class="toggle-button">
+        <span class="bar"></span>
+        <span class="bar"></span>
             <div class="navbar-links">
                 <ul>
-                    <li><a href="../index.php">Home</a></li>
-                    <li><a href="../highscore.php">Hi-Score</a></li>
-                    <li><a href="../games.php">Games</a></li>
-                    <li><a href="../inloggen.php">Login</a></li>
+                    <li><a href="./index.php">Home</a></li>
+                    <li><a href="./highscore.php">Hi-Score</a></li>
+                    <li><a href="./games.php">Games</a></li>
+                    <li><a href="./inloggen.php">Login</a></li>
                 </ul>
         </nav>
         </div>
