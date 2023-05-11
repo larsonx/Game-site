87c2fed99930b1edb6f6be277e083149582cdd77
larsonx <97100495@st.deltion.nl>
2023-05-11 10:54:41 +0200

    fix

--- header.php
+++ header.php
@@ -1,11 +1,12 @@
 <header>
         <nav class="navbar">
-            <div class="brand-title">GameHub</div>
-            <a href="#" class="toggle-button">
-                <span class="bar"></span>
-                <span class="bar"></span>
-                <span class="bar"></span>
-            </a>
+        <div class="brand-title">
+        <span class="span1">Game</span>
+        <span class="span2">Hub</span>
+      </div>
+      <a href="#" class="toggle-button">
+        <span class="bar"></span>
+        <span class="bar"></span>
             <div class="navbar-links">
                 <ul>
                     <li><a href="./index.php">Home</a></li>
