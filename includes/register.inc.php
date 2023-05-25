7b9cecf10fc3a5dfcd3b259f225c096dbefd7e27
larson cop <97100495@st.deltion.nl>
2023-05-25 14:40:48 +0200

    update

--- includes/register.inc.php
+++ includes/register.inc.php
@@ -5,7 +5,29 @@ $name = $_POST["name"];
 $email = $_POST["email"];
 $username = $_POST["uid"];
 $pwd = $_POST["pwd"];
+
+require_once 'dbh.inc.php';
+require_once 'functions.inc.php';
+
+if (emptynputRegister($name, $email, $username, $pwd) !== false){
+    header("location: ../Inloggen.php?error=emptyinput");
+exit();
 }
+if (invalidUid($username) !== false){
+    header("location: ../Inloggen.php?error=invaliduid");
+exit();
+}
+if (invalidEmail($email) !== false){
+    header("location: ../Inloggen.php?error=invalidemail;");
+exit();
+}
+if (uidExists($conn, $username) !== false){
+    header("location: ../Inloggen.php?error=usernametaken;");
+exit();
+}
+
+
 else{
     header("location: ../Inloggen.php");
+}
 }
