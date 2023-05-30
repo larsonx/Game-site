260424ee0bcc897a6305fbaab59672443fe30847
larsonx <97100495@st.deltion.nl>
2023-05-30 11:09:40 +0200

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -3,7 +3,7 @@
 $serverName = "localhost";
 $dBUsername = "root";
 $dBPassword = "";
-$dBName = "gameHub";
+$dBName = "usersgamehub";
 
 $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
 
