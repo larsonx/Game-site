6cdb32554c1ff1a48c3e21445a3a3907fa29ffc3
larson cop <97100495@st.deltion.nl>
2023-06-07 14:51:04 +0200

    Update Profiel.php

--- Profiel.php
+++ Profiel.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
+
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE-edge" />
@@ -8,24 +9,16 @@
     <script src="script.js" defer></script>
     <title>GameHub</title>
 </head>
+
 <body>
 
-<?php
-include 'header.php';
-  ?>
-        <div class="container">
-            <div class="profile-header">
-                <div class="profile-image">
-                    <img src="https://via.placeholder.com/150" alt="Profile Image">
-                </div>
-                <div class="profile-username">
-                    <h1>Username</h1>
-                </div>
-            </div>
-            <div class="profile-bio">
-                <h2>Biography</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at orci sit amet augue mollis vestibulum ac vitae elit. Nulla auctor aliquet nisi, eu pellentesque neque pharetra in.</p>
-            </div>
-        </div>
+    <?php
+    include 'header.php';
+    ?>
+    <div class="container">
+
+
+</div>
 </body>
+
 </html>
