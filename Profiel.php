a9489219f9419bf636b5f094e720c7d77f6e6778
larsonx <97100495@st.deltion.nl>
2023-06-07 19:46:43 +0200

    Update Profiel.php

--- Profiel.php
+++ Profiel.php
@@ -1,9 +1,5 @@
-<?php
-include_once 'includes/dbh.inc.php';
-?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE-edge" />
@@ -12,16 +8,32 @@ include_once 'includes/dbh.inc.php';
     <script src="script.js" defer></script>
     <title>GameHub</title>
 </head>
-
 <body>
 
-    <?php
-    include 'header.php';
+<?php
+include 'header.php';
+  ?>
+        <div class="container">
+            <div class="profile-header">
+                <div class="profile-image">
+                <img src="https://via.placeholder.com/150" alt="Profile Picture">
+        <form action="upload.php" method="POST" enctype="multipart/form-data">
+            <input type="file" name="profile-image">
+            <input type="submit" name="upload" value="Upload Image">
+        </form>
+                </div>
+                <div class="profile-username">
+                    <h1>Username</h1>
+                </div>
+            </div>
+            <div class="profile-bio">
+            <h2>Biography</h2>
+        <form action="update_bio.php" method="POST">
+            <textarea name="biography"></textarea>
+            <input type="submit" name="save-bio" value="Save">
+        </form>
 
-    ?>
-    
-
-
+        </div>
+        </div>
 </body>
-
 </html>
