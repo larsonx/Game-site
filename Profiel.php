bf97e2042c04208dccf1e2a54a1da1ad969603e8
larson cop <97100495@st.deltion.nl>
2023-06-14 12:39:23 +0200

    Update Profiel.php

--- Profiel.php
+++ Profiel.php
@@ -11,39 +11,39 @@
 </head>
 
 <body>
+<?php
+include 'header.php';
 
-    <?php
-    include 'header.php';
+?>
 
-    if (isset($_SESSION['bio'])) {
-        $biography = $_SESSION['bio'];
-        // Display the biography on the profile page
-        echo $biography;
-      }
-      
-   
-
-    ?>
 
     <div class="container">
         <div class="profile-header">
             <div class="profile-image">
-            <img src="uploads/<?php echo $profileImage; ?>" alt="Profile Image">
+            <img src="<?php echo $profileImage; ?>" alt="Profile Image">
+
                 <form action="profile_update.php" method="POST" enctype="multipart/form-data">
-                    <input type="file" name="profileImage"  id="profileImage">
+                    <input type="file" name="profileImage" id="profileImage">
             </div>
             <div class="profile-username">
                 <h1>Username</h1>
             </div>
         </div>
         <div class="profile-bio">
-        <label for="bio"><h2>Biography:<h2></label>
-               
-                <textarea name="bio" id="bio"></textarea>
-                  <input type="submit" value="Update Profile">
-            
-         
+            <label for="bio">
+                <h2>Biography:<h2>
+            </label>
+           <?php
+if (isset($_SESSION['bio'])) {
+    $biography = $_SESSION['bio'];
+    // Display the biography on the profile page
+    echo $biography;
+}
+           ?>
+            <textarea name="bio" id="bio"></textarea>
+            <input type="submit" value="Update Profile">
+
+
         </div>
     </div>
 </body>
-
