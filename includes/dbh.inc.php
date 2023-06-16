d62e2e296b4198eac8d8b68404f17011d19b3653
Danila_gruznev <302860771@student.rocmondriaan.nl>
2023-06-16 11:37:28 +0200

    top

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -3,12 +3,12 @@
 $serverName = "localhost";
 $dBUsername = "root";
 $dBPassword = "";
-$dBName = "gamehub";
+$dBName = "test";
 
 $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
 
 if (!$conn) {
-die("Connection failed:" . mysqli_connect_error());
+    die("Connection failed:" . mysqli_connect_error());
 }
 
 
