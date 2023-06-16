8aa61739167939d4e9065c9e299666b73fd5a023
Danila_gruznev <302860771@student.rocmondriaan.nl>
2023-06-16 14:21:07 +0200

    idk

--- index.php
+++ index.php
@@ -14,45 +14,10 @@
   <?php
   include 'header.php';
   require_once 'includes/dbh.inc.php';
+  ?>
 
-  // Database connection setup
-  // $serverName = "localhost";
-  // $dBUsername = "root";
-  // $dBPassword = "";
-  // $dBName = "test";
-  
-  // try {
-  //   $pdo = new PDO("mysql:host=$serverName;dbname=$dBName", $dBUsername, $dBPassword);
-  //   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  // } catch (PDOException $e) {
-  //   die("Database connection failed: " . $e->getMessage());
-  // }
-  
-  // // Check if the user is logged in
-  // if (isset($_SESSION['userId'])) {
-  //   $userId = $_SESSION['userId'];
-  
-  //   // Retrieve user data from the database
-  //   $query = "SELECT usersName FROM users WHERE usersId='$userId'";
-  //   $stmt = $pdo->prepare($query);
-  //   $stmt->bindParam(':userId', $userId, PDO::PARAM_INT);
-  //   $stmt->execute();
-  //   $user = $stmt->fetch(PDO::FETCH_ASSOC);
-  
-  //   // Check if a user with the provided ID exists
-  //   if ($user) {
-  //     $welcomeMessage = "Welcome, " . $user['usersName'] . "!"; // Customize the welcome message as per your needs
-  //   } else {
-  //     $welcomeMessage = "Welcome!";
-  //   }
-  // } else {
-  //   $welcomeMessage = "Welcome!";
-  // }
-  
-  // // Display the welcome message
-  // echo $welcomeMessage;
-  // ?>
-
+  <h1 class="welcome-field">Welcome to the GameHub</span>
+  </h1>
 </body>
 
 </html>
