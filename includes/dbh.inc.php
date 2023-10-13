3bee2514f151d799c3c36de61e29006ca71c825c
Danila_gruznev <302860771@student.rocmondriaan.nl>
2023-10-13 14:10:11 +0200

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -3,7 +3,7 @@
 $serverName = "localhost";
 $dBUsername = "root";
 $dBPassword = "";
-$dBName = "gamehub";
+$dBName = "test";
 
 $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
 
